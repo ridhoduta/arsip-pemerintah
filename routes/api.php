8f27ba6d0c8932701f6641cadf2b584ec434d509
ridhoduta <ridhodutay@gmail.com>
2025-09-08 07:23:50 +0700

    perubahan route

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::get('/halo', function () {
+Route::get('/haloo', function () {
     return response()->json(['message' => 'Halo dari API Laravel!']);
 });
