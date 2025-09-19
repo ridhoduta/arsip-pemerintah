7f5a4307f6ea6985bdf98f24deb863a460cecaa9
ridhoduta <ridhodutay@gmail.com>
2025-09-20 00:41:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuratController;
 
-Route::get('/app/{any?}', function () {
+Route::get('/', function () {
+    return redirect('/app/surats');
+});
+
+// Catch-all route untuk React
+Route::get('/app/{any}', function () {
     return file_get_contents(public_path('react/index.html'));
 })->where('any', '.*');
